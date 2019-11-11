3686a12404ac82e53fc8e0583d2056e0a89053be
Daniel Martinez <damavi84@gmail.com>
2019-11-11 21:52:30 +0100

    cambio de rango en valor aleatorio

--- src/Aciertos.java
+++ src/Aciertos.java
@@ -4,8 +4,8 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
-        //System.out.print(x);
+        int x = (int) (Math.floor(Math.random()*(50-10+1)+10));
+        System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
